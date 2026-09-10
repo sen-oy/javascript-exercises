405fb9e5c00b2bc58e71909888492aedc09b792c
sen-oy <313918650+sen-oy@users.noreply.github.com>
2026-09-10 13:30:22 -0400

    Complete sumAll function and enable all tests.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    if (firstNumber < 0 || secondNumber < 0) {
+        return "ERROR";
+    }
 
+    if (!Number.isInteger(firstNumber) || !Number.isInteger(secondNumber)) {
+        return "ERROR";
+    }
+    
+    let lowerBound, upperBound;
+    let sum = 0;
+    if (firstNumber < secondNumber) {
+        lowerBound = firstNumber;
+        upperBound = secondNumber;
+    } else {
+        lowerBound = secondNumber;
+        upperBound = firstNumber;
+    }
+
+    for (let i = lowerBound; i <= upperBound; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
