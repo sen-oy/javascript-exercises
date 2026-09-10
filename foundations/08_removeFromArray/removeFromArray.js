6c7145044f9df9c4d7aeb37ae70ca4bcf35ce7be
sen-oy <313918650+sen-oy@users.noreply.github.com>
2026-09-10 12:16:26 -0400

    Complete removeFromArray function and enabled all tests.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...theArguments) {
+    for (let i = 0; i < inputArray.length; i++) {
+        for (let j = 0; j < theArguments.length; j++) {
+            if (inputArray[i] === theArguments[j]) {
+                inputArray.splice(inputArray.indexOf(theArguments[j]), 1);
+                i--;
+            }
+        }
+    }
+    return inputArray;
 };
+
+// take an array and any number of additional arguments
+// from that array remove the additional arguments
+// return the resultant array
 
 // Do not edit below this line
 module.exports = removeFromArray;
