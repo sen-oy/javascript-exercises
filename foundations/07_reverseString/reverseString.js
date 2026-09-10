1522b4017a8aad826ca96c110a151d47dfc3b77c
sen-oy <313918650+sen-oy@users.noreply.github.com>
2026-09-10 11:24:27 -0400

    Complete reverseString function and enable all tests.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let returnString = '';
+    for (let i = inputString.length - 1; i >= 0; i--) {
+        returnString += inputString[i];
+    }
+    return returnString;
 };
+
+// takes a string
+// reverses the string
+// returns the string
 
 // Do not edit below this line
 module.exports = reverseString;
