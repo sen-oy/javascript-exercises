470c3f7473e1593ede4127a1efc0779497d8e326
sen-oy <313918650+sen-oy@users.noreply.github.com>
2026-09-10 13:49:25 -0400

    Complete tempConversion functions and enabled all tests.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemperature) {
+  // fahrenheit - 32 * 5/9
+  // round to 1 decimal place
+  let celsiusTemperature = (fahrenheitTemperature - 32) * (5 / 9);
+  return round(celsiusTemperature, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemperature) {
+  // formula => celsius * (9/5) + 32
+  // round to 1 decimal place
+  let fahrenheitTemperature = (celsiusTemperature * (9 / 5)) + 32;
+  return round(fahrenheitTemperature, 1);
 };
+
+function round(value, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
 
 // Do not edit below this line
 module.exports = {
