0139b0a42e849e150794d789491d58100a06bfe1
sen-oy <313918650+sen-oy@users.noreply.github.com>
2026-09-22 19:38:24 -0400

    Complete function definition and enable all tests.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let fibonacciArray = [1, 1];
+    index = parseInt(index);
+    if (index < 0 ) {
+        return 'OOPS';
+    } else if (index === 0) {
+        return 0;
+    } else if (index < 3) {
+        return index == 1 ? fibonacciArray[0] : fibonacciArray[1];
+    } else {
+        for (let i = 2; i < index; i++) {
+            fibonacciArray.push(fibonacciArray[i - 2] + fibonacciArray[i - 1]);
+        }
+        return fibonacciArray[index - 1];
+    }
 
 };
 
