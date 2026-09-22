9d22dd81e645d3159b2581c55cdf455b04fad762
sen-oy <313918650+sen-oy@users.noreply.github.com>
2026-09-22 18:22:17 -0400

    Complete function definitions and enabled all tests.

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numberArray) {
+	return numberArray.reduce((accumulator, current) => accumulator + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numberArray) {
+  return numberArray.reduce((accumulator, current) => accumulator * current, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let product = 1;
+  for (let i = number; i > 1; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
