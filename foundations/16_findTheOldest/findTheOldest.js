a35f844730ac8294c81739302a6b00b2d02d0fbb
sen-oy <313918650+sen-oy@users.noreply.github.com>
2026-09-22 20:06:20 -0400

    Complete function definition and enable all tests.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArray) {
+    let ordered = peopleArray.sort((personA, personB) => {
+        let ageA = (personA.yearOfDeath || (new Date().getFullYear())) - personA.yearOfBirth;
+        let ageB = (personB.yearOfDeath || (new Date().getFullYear())) - personB.yearOfBirth;
+        return ageA < ageB ? 1 : -1;
+    })
+    return ordered[0];
 };
 
 // Do not edit below this line
